01a27cdf3cec0e8809e592d53be48a84a7ac1014
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-09-18 17:36:15 +0300

    registration form

--- src/app/components/inputs/inputs.tsx
+++ src/app/components/inputs/inputs.tsx
@@ -1,38 +1,44 @@
-'use client'
+"use client";
 
 interface InputsProps {
-    type: string,
-    vlaue: string,
-    onChange :(event: React.ChangeEvent<HTMLInputElement>) => void,
-    name: string,
-    textarea: boolean,
-    id: string,
-    placeholder: string,
-    big: boolean,
-
+  type: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+  textarea: boolean;
+  id: string;
+  placeholder: string;
+  big: boolean;
 }
 
 export default function Inputs({
-    type ,
-    vlaue ,
-    onChange ,
-    name,
-    textarea,
-    id,
-    placeholder,
-    big,
+  type,
+  value,
+  onChange,
+  name,
+  textarea,
+  id,
+  placeholder,
+  big,
 }: InputsProps) {
-    return (
-         <input 
-         type={type} 
-         placeholder={placeholder} 
-         id={id}
-         value={vlaue}
-         onChange={onChange}
-        name={name}
-        
-        className=""
-         /> 
-         
-    ) 
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      id={id}
+      value={value}
+      onChange={onChange}
+      name={name}
+      className={`w-full
+         p-4
+          pt-6 
+          font-light
+           bg-light
+            border-2 
+            outline-none
+             text-black 
+             ${textarea ? "w-700px h-500px" : "w-full"} 
+              ${big ? "w-[700] pb-[1rem]" : ""} `}
+    />
+  );
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,32 @@
-import React from 'react'
+'use client'
+import React, { useState }  from 'react';
+import { FormEvent } from 'react';
+import Inputs from '../components/Inputs/Inputs';
+
+
+const initalState = {
+  name: '',
+  email: '',
+  password: ''
+}
+
 
 export default function page()  {
+const [state,setState] = useState(initalState)
+
+
+function handleChange (event: any) {
+ setState ({...state , [event.target.name]: event.target.value})
+}
+
   return (
  <form className='text-center'>
      <div className=' flex flex-col justify-center  h-[450px] w-[350px] mx-auto gap-2 '>
-          <p>1</p>
-          <p>2</p>
-          <p>3</p>
-          <p>4</p>
-
+       <Inputs placeholder='Name' id='name' type='text' name='name' onChange={handleChange} value={state.name}/>
+       <Inputs placeholder='Email' id='email' type='text' name='email' onChange={handleChange} value={state.name}/>
+       <Inputs placeholder='Password' id='password' type='password' name='password' onChange={handleChange} value={state.name}/>
+     
+      <button type='submit' className='bg-blue-500 text-white p-2 rounded-md'>Register</button>
       </div>
  </form>
   )
