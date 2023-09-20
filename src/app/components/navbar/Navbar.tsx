cf2ad63ab62cb6e0f1c489a1919b292e42d1a8a2
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-09-20 19:05:21 +0300

    implementing nav bar

---
+++ src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+'use client'
+import { Link } from '@chakra-ui/react'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='shadow-xl bg-white z-[-9999] sticky px-2'>
+        <div className=' flex items-center justify-between gap-2'>
+            <div className='flex items-center gap-6 flex-1 relative'>
+                 <Link href='/'><img src="/logo.svg" alt='Logo' width={91} height={34}></img></Link>
+
+                 <form className='lg: flex-1 lg:flex hidden '>
+                    <input type="text"
+                    className='w-full p-3 font-light bg-white rounded-full border-black border-[1px] outline-none'
+                    />
+                 </form>
+            </div>
+
+           <div className='items-center gap-4 text-[.8rem] px-2 hidden lg:flex'>
+              <div>
+                <Link href='/'>Udemy Buisnes</Link>
+              </div>
+
+              <div>
+                <Link href='/'>Teach on Udemy</Link>
+              </div>
+        </div>
+
+
+
+        </div>
+      
+    </div>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Loading from './loading'
-
+import Navbar from './components/navbar/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -20,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
        {/* <Loading/> */}
+       <Navbar/>
         {children}
         </body>
     </html>
