2def31eaa8e4a398bfb2d968bc65826041f2f8ff
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-09-26 18:27:13 +0300

    Updated course.ts

--- src/app/api/course/route.ts
+++ src/app/api/course/route.ts
@@ -4,4 +4,35 @@ import myUser from '@/app/actions/getUser'
 
 export async function POST (request: Request){
     const currentUser = await myUser();
+
+
+    if (!currentUser) {
+        return console.log('No user found')     
+    }
+
+    const body = await request.json();
+     
+
+    const {
+        name,
+        author, 
+        imageSrc,
+        description,
+        price,
+    }= body;
+
+
+    const course = await prisma.course.create({     
+
+        data: {
+            name,
+            author,
+            imageSrc,
+            description,
+            price,
+            userId: currentUser.id,
+        }
+    })
+
+    return NextResponse.json(course)
 }
