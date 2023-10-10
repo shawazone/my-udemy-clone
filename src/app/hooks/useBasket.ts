f577a800781be98d6a4b0d5531688bf287a54865
mohamad alshawa <mohamad.alchawwa+github@gmail.com>
2023-10-10 18:29:28 +0300

    implemented useBasket hook

--- src/app/hooks/useBasket.ts
+++ src/app/hooks/useBasket.ts
@@ -14,7 +14,31 @@ const userBasket = ({courseId, currentUser}:IUseBasket) => {
     const router = useRouter();
 
     const hasBasket = useMemo(() => {
-        const List = 
-    })
+        const List = currentUser?.basketIds || [];
+        return List.includes(courseId);
+    },[currentUser,courseId]);
+
+    const toggleBasket = useCallback(async (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        e.stopPropagation();
+            try{
+                let request ;
+
+                if(hasBasket){
+                    request = () => axios.delete(`/api/basket/${courseId}`)
+                }else {
+                    request = ()=> axios.post(`/api/basket/${courseId}`)
+                }
+
+                await request();
+                router.refresh();
+
+            }catch(error:any){
+                throw new Error(error);
+            }
+    },[currentUser,hasBasket,courseId, router])
+
+    return{
+         hasBasket,toggleBasket
+    }
 
 }
